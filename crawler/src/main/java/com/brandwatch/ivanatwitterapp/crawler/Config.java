f7b0f6e502b538adfefee0b6aea4fa2f86591679
Ivana Koceva <ikoceva@brandwatch.com>
2020-05-28 12:51:10 +0200

    Remove spring social after rebase

--- crawler/src/main/java/com/brandwatch/ivanatwitterapp/crawler/Config.java
+++ crawler/src/main/java/com/brandwatch/ivanatwitterapp/crawler/Config.java
@@ -1,23 +1,19 @@
 package com.brandwatch.ivanatwitterapp.crawler;
 
-import com.brandwatch.ivanatwitterapp.common.repositories.MongoMentionRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.social.twitter.api.Twitter;
-import org.springframework.social.twitter.api.impl.TwitterTemplate;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
 
 @Configuration
 @EnableScheduling
 public class Config {
-    
-    @Bean
-    public Twitter twitter(final @Value("${spring.social.twitter.appId}") String appId,
-                           final @Value("${spring.social.twitter.appSecret}") String appSecret) {
 
-        return new TwitterTemplate(appId, appSecret);
+    @Bean
+    public Twitter getTwitterInstance() {
+        return TwitterFactory.getSingleton();
     }
 }
 
